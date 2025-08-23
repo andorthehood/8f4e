4a43f40021ffa86ec4e33ac51ca49381dd03a961
andorthehood <andorsigned@gmail.com>
2025-08-23 13:25:31 +0200

    feat: update typescript and fix type errors

---
+++ packages/editor/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/*.(test|spec).+(ts|tsx|js)'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    setupFilesAfterEnv: ['../../jest.image.ts'],
+}; 
