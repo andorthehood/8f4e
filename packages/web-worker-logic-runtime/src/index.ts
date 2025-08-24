d0c4f3fddd4658f5991f66ab8695957e41a5e3d2
andormade <andorsigned@gmail.com>
2025-08-25 00:51:59 +0200

    feat: add logic runtime

---
+++ packages/web-worker-logic-runtime/src/index.ts
@@ -0,0 +1,56 @@
+import createModule from './createModule';
+
+let interval: NodeJS.Timeout;
+let statsInterval: NodeJS.Timeout;
+let timeToExecute: number;
+let lastIntervalTime: number;
+let drift = 0;
+
+async function init(memoryRef: WebAssembly.Memory, sampleRate: number, codeBuffer: Uint8Array) {
+	try {
+		clearInterval(interval);
+		clearInterval(statsInterval);
+
+		const wasmApp = await createModule(memoryRef, codeBuffer);
+
+		const intervalTime = Math.floor(1000 / sampleRate);
+
+		interval = setInterval(() => {
+			const startTime = performance.now();
+			drift += intervalTime - (startTime - lastIntervalTime);
+			lastIntervalTime = startTime;
+			wasmApp.cycle();
+			const endTime = performance.now();
+			timeToExecute = endTime - startTime;
+		}, intervalTime);
+
+		statsInterval = setInterval(() => {
+			self.postMessage({
+				type: 'stats',
+				payload: {
+					drift,
+					timeToExecute,
+				},
+			});
+		}, 10000);
+
+		self.postMessage({
+			type: 'initialized',
+			payload: {},
+		});
+	} catch (error) {
+		console.log('buildError', error);
+		self.postMessage({
+			type: 'buildError',
+			payload: error,
+		});
+	}
+}
+
+self.onmessage = function (event) {
+	switch (event.data.type) {
+		case 'init':
+			init(event.data.payload.memoryRef, event.data.payload.sampleRate, event.data.payload.codeBuffer);
+			break;
+	}
+};
