0d7c0155b615d60dd3ebbdb11ea58980a320e3a2
andorthehood <hngrhorace@gmail.com>
2025-08-26 10:15:37 +0200

    fix: audio worklet loading

---
+++ packages/audio-worklet-runtime/vite.config.worklet.ts
@@ -0,0 +1,24 @@
+import path from 'node:path';
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	build: {
+		outDir: 'dist',
+		emptyOutDir: true,
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			formats: ['es'],
+			fileName: () => 'index.js', // single output file
+		},
+		rollupOptions: {
+			output: {
+				inlineDynamicImports: true,
+				manualChunks: undefined,
+			},
+			external: [], // inline your own deps
+		},
+		sourcemap: true,
+		minify: false, // easier to debug
+	},
+});
