719b34a69257bfe62520c7d58671e66d8e7a8560
andorthehood <hngrhorace@gmail.com>
2025-10-17 21:15:30 +0200

    chore(editor): refactor event names

--- packages/editor/src/state/effects/colorTheme.ts
+++ packages/editor/src/state/effects/colorTheme.ts
@@ -5,7 +5,7 @@ import type { State } from '../types';
 export default function colorTheme(state: State, events: EventDispatcher): () => void {
 	function onSetColorScheme({ colorScheme }) {
 		state.editorSettings.colorScheme = colorScheme;
-		events.dispatch('saveState');
+		events.dispatch('saveEditorSettings');
 	}
 
 	events.on('setColorScheme', onSetColorScheme);
