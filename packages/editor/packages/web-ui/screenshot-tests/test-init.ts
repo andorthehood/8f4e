bcddd548e0a58b24155fa2dd30d7fa58517629e5
andorthehood <hngrhorace@gmail.com>
2025-10-23 14:26:32 +0200

    feat(web-ui): test colors

--- packages/editor/packages/web-ui/screenshot-tests/test-init.ts
+++ packages/editor/packages/web-ui/screenshot-tests/test-init.ts
@@ -2,6 +2,7 @@ import init from '@8f4e/web-ui';
 
 import generateCodeBlockMock from './utils/generateCodeBlockMock';
 import generateStateMock from './utils/generateStateMock';
+import generateColorMapMock from './utils/generateColorMapMock';
 
 async function initializeWebUI() {
 	const canvas = document.getElementById('test-canvas') as HTMLCanvasElement;
@@ -9,8 +10,26 @@ async function initializeWebUI() {
 		throw new Error('Canvas element not found');
 	}
 
-	const mockState = generateStateMock(generateCodeBlockMock());
+	const mockState = generateStateMock();
 	const webUI = await init(mockState, canvas);
+
+	if (mockState.graphicHelper.spriteLookups) {
+		mockState.graphicHelper.activeViewport.codeBlocks.add(
+			generateCodeBlockMock(
+				[
+					'',
+					'lorem ipsum dolor sit amet',
+					'consectetur adipiscing elit',
+					'sed do eiusmod tempor',
+					'ut enim ad minim veniam',
+					'quis nostrud exercitation',
+					'',
+				],
+				generateColorMapMock(mockState.graphicHelper.spriteLookups)
+			)
+		);
+	}
+
 	console.log('Web-UI initialized:', webUI);
 	return webUI;
 }
---
+++ packages/editor/packages/web-ui/screenshot-tests/utils/generateColorMapMock.ts
@@ -0,0 +1,17 @@
+import { SpriteLookups } from '@8f4e/sprite-generator';
+
+export default function (spriteLookups: SpriteLookups) {
+	return [
+		[spriteLookups.fontBinaryOne],
+		[spriteLookups.fontBinaryZero],
+		[spriteLookups.fontCode],
+		[spriteLookups.fontCodeComment],
+		[spriteLookups.fontDialogText],
+		[spriteLookups.fontDialogTitle],
+		[spriteLookups.fontInstruction],
+		[spriteLookups.fontLineNumber],
+		[spriteLookups.fontMenuItemText],
+		[spriteLookups.fontMenuItemTextHighlighted],
+		[spriteLookups.fontNumbers],
+	];
+}
