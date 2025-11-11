d441d51461abfaf5cba3445a3a4f8d389b87e20d
andormade <hngrhorace@gmail.com>
2025-11-11 21:29:32 +0000

    feat(editor): add history tracking

---
+++ packages/editor/packages/editor-state/src/effects/historyTracking.ts
@@ -0,0 +1,31 @@
+import { StateManager } from '@8f4e/state-manager';
+
+import { EventDispatcher } from '../types';
+import { serializeToProject } from '../helpers/projectSerializer';
+
+import type { State } from '../types';
+
+export default function historyTracking(store: StateManager<State>, events: EventDispatcher): void {
+	const state = store.getState();
+	if (!state.featureFlags.historyTracking) {
+		return;
+	}
+
+	function onSaveHistory() {
+		if (state.history.length >= 10) {
+			state.history.shift();
+		}
+
+		state.history.push(serializeToProject(state));
+	}
+
+	function undo() {
+		const previousState = state.history.pop();
+		if (previousState) {
+			events.dispatch('loadProject', { project: previousState });
+		}
+	}
+
+	store.subscribe('graphicHelper.selectedCodeBlock.code', onSaveHistory);
+	events.on('undo', undo);
+}
