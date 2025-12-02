26667435e0d31f3721e7b5a3de66a09b2cf0c728
andorthehood <andorsigned@gmail.com>
2025-12-02 13:19:09 +0000

    chore(editor-state): splitting code editing helpers

--- packages/editor/packages/editor-state/src/effects/codeBlocks/codeBlockDecorators/buttons/updateGraphicData.ts
+++ packages/editor/packages/editor-state/src/effects/codeBlocks/codeBlockDecorators/buttons/updateGraphicData.ts
@@ -1,6 +1,6 @@
 import { parseButtons } from './codeParser';
 
-import { gapCalculator } from '../../../../helpers/codeEditing/editor';
+import { gapCalculator } from '../../../../helpers/codeEditing/gapCalculator';
 
 import type { CodeBlockGraphicData, State } from '../../../../types';
 
--- packages/editor/packages/editor-state/src/effects/codeBlocks/codeBlockDecorators/switches/updateGraphicData.ts
+++ packages/editor/packages/editor-state/src/effects/codeBlocks/codeBlockDecorators/switches/updateGraphicData.ts
@@ -1,6 +1,6 @@
 import { parseSwitches } from './codeParser';
 
-import { gapCalculator } from '../../../../helpers/codeEditing/editor';
+import { gapCalculator } from '../../../../helpers/codeEditing/gapCalculator';
 
 import type { CodeBlockGraphicData, State } from '../../../../types';
 
