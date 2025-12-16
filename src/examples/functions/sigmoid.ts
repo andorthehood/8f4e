0bf8900073ab6456798387af7717fda4b6f99bd9
andorthehood <andorsigned@gmail.com>
2025-12-16 16:48:20 +0000

    feat(examples): add example functions

---
+++ src/examples/functions/sigmoid.ts
@@ -0,0 +1,24 @@
+import type { ExampleModule } from '@8f4e/editor-state';
+
+const sine: ExampleModule = {
+	title: 'Sigmoid (Polynomial Approximation)',
+	author: 'Andor Polgar',
+	category: 'Functions',
+	code: `function sigmoid
+; Sigmoid function approximation
+; using a polynomial
+param float x
+
+localGet x
+dup
+abs
+push 1.0
+add
+ensureNonZero'
+div
+
+functionEnd float`,
+	tests: [],
+};
+
+export default sine;
