ffaa90df2ab0984373977c20a63353ebb7a3d33c
andorthehood <hngrhorace@gmail.com>
2025-12-21 12:17:39 +0000

    feat(compiler): add support for anonymous allocations

--- packages/editor/packages/editor-state/src/effects/codeBlocks/codeBlockDecorators/outputs/codeParser.ts
+++ packages/editor/packages/editor-state/src/effects/codeBlocks/codeBlockDecorators/outputs/codeParser.ts
@@ -6,7 +6,19 @@ export function parseOutputs(code: string[]) {
 			const [, instruction, ...args] = (line.match(instructionParser) ?? []) as [never, string, string, string];
 
 			if (instruction === 'int' || instruction === 'float' || instruction === 'int[]' || instruction === 'float[]') {
-				return [...acc, { id: args[0], lineNumber: index }];
+				if (!args[0]) {
+					return acc;
+				}
+
+				let id: string;
+				// If args[0] is a number or starts with a capital letter, it's an anonymous memory allocation.
+				if (!isNaN(Number(args[0])) || (args[0][0] >= 'A' && args[0][0] <= 'Z')) {
+					id = '__anonymous__' + index;
+				} else {
+					id = args[0];
+				}
+
+				return [...acc, { id, lineNumber: index }];
 			}
 			return acc;
 		},
