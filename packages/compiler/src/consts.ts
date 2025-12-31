2d5b5dabad645299be5eaecba1bb28c4acaa773f
andorthehood <hngrhorace@gmail.com>
2025-12-31 22:46:50 +0000

    chore(compiler): relocating constants

--- packages/compiler/src/consts.ts
+++ packages/compiler/src/consts.ts
@@ -5,3 +5,7 @@ export const I16_SIGNED_SMALLEST_NUMBER = -32768;
 export const LOGIC_HIGH = I16_SIGNED_LARGEST_NUMBER;
 export const LOGIC_LOW = 0;
 export const GLOBAL_ALIGNMENT_BOUNDARY = 4;
+export const HEADER = [0x00, 0x61, 0x73, 0x6d];
+export const VERSION = [0x01, 0x00, 0x00, 0x00];
+// Number of exported WASM functions (init, cycle, buffer)
+export const EXPORTED_FUNCTION_COUNT = 3;
