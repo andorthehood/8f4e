70104c8317b280e731177c71cf9ce1d15ee16ffc
andorthehood <hngrhorace@gmail.com>
2026-01-02 16:04:23 +0000

    feat(editor-state): extend config schema

--- packages/editor/packages/editor-state/src/configSchema.ts
+++ packages/editor/packages/editor-state/src/configSchema.ts
@@ -8,7 +8,88 @@ const runtimeSettingsSchema: ConfigSchema = {
 			enum: ['WebWorkerLogicRuntime', 'MainThreadLogicRuntime', 'AudioWorkletRuntime', 'WebWorkerMIDIRuntime'],
 		},
 		sampleRate: { type: 'number' },
+		audioInputBuffers: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					memoryId: { type: 'string' },
+					channel: { type: 'number' },
+					input: { type: 'number' },
+				},
+				additionalProperties: false,
+			},
+		},
+		audioOutputBuffers: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					memoryId: { type: 'string' },
+					channel: { type: 'number' },
+					output: { type: 'number' },
+				},
+				additionalProperties: false,
+			},
+		},
+		midiNoteOutputs: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					moduleId: { type: 'string' },
+					channelMemoryId: { type: 'string' },
+					portMemoryId: { type: 'string' },
+					velocityMemoryId: { type: 'string' },
+					noteOnOffMemoryId: { type: 'string' },
+					noteMemoryId: { type: 'string' },
+				},
+				additionalProperties: false,
+			},
+		},
+		midiNoteInputs: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					moduleId: { type: 'string' },
+					channelMemoryId: { type: 'string' },
+					portMemoryId: { type: 'string' },
+					velocityMemoryId: { type: 'string' },
+					noteOnOffMemoryId: { type: 'string' },
+					noteMemoryId: { type: 'string' },
+				},
+				additionalProperties: false,
+			},
+		},
+		midiControlChangeOutputs: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					moduleId: { type: 'string' },
+					channelMemoryId: { type: 'string' },
+					selectedCCMemoryId: { type: 'string' },
+					valueMemoryId: { type: 'string' },
+				},
+				additionalProperties: false,
+			},
+		},
+		midiControlChangeInputs: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					moduleId: { type: 'string' },
+					channelMemoryId: { type: 'string' },
+					selectedCCMemoryId: { type: 'string' },
+					valueMemoryId: { type: 'string' },
+				},
+				additionalProperties: false,
+			},
+		},
 	},
+	additionalProperties: false,
 };
 
 const configSchema: ConfigSchema = {
@@ -24,6 +105,7 @@ const configSchema: ConfigSchema = {
 			items: runtimeSettingsSchema,
 		},
 	},
+	additionalProperties: false,
 };
 
 export default configSchema;
