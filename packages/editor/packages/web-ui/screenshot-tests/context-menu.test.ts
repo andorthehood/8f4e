2b2d179b76ea1172187b8b3ecf07f56445c19684
andorthehood <hngrhorace@gmail.com>
2026-01-07 21:57:17 +0000

    chore(editor): move memory view manager into the editor

--- packages/editor/packages/web-ui/screenshot-tests/context-menu.test.ts
+++ packages/editor/packages/web-ui/screenshot-tests/context-menu.test.ts
@@ -4,13 +4,13 @@ import init from '@8f4e/web-ui';
 import createMockStateWithColors from './utils/createMockStateWithColors';
 import generateContextMenuMock from './utils/generateContextMenuMock';
 import createCanvas from './utils/createCanvas';
-import createMockMemoryRef from './utils/createMockMemoryRef';
+import createMockMemoryViews from './utils/createMockMemoryViews';
 
 test('context menu', async () => {
 	const canvas = createCanvas();
 	const mockState = createMockStateWithColors();
-	const memoryRef = createMockMemoryRef();
-	await init(mockState, canvas, memoryRef);
+	const memoryViews = createMockMemoryViews();
+	await init(mockState, canvas, memoryViews);
 	mockState.graphicHelper.contextMenu = generateContextMenuMock();
 	await expect(canvas).toMatchScreenshot();
 });
