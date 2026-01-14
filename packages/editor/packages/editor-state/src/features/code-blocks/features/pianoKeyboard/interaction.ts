6448be02a87564c742b8fc57ffc5af2930be1674
andorthehood <hngrhorace@gmail.com>
2026-01-14 20:29:39 +0000

    chore(editor-state): reorganise files within code-blocks feature

---
+++ packages/editor/packages/editor-state/src/features/code-blocks/features/pianoKeyboard/interaction.ts
@@ -0,0 +1,78 @@
+import { StateManager } from '@8f4e/state-manager';
+
+import findPianoKeyAtViewportCoordinates from './findPianoKeyboardAtViewportCoordinates';
+
+import { CodeBlockClickEvent } from '../codeBlockDragger/effect';
+import { EventDispatcher } from '../../../../types';
+import insertCodeAfterLine from '../../utils/codeParsers/insertCodeAfterLine';
+import replaceCode from '../../utils/codeParsers/replaceCode';
+
+import type { State } from '../../../../types';
+
+function generateCode(
+	pressedKeys: Set<number>,
+	pressedKeysListMemoryId: string,
+	isInteger: boolean,
+	startingNumber: number
+) {
+	return Array.from(pressedKeys).flatMap((key, index) => {
+		const value = key + startingNumber;
+		return [`init ${pressedKeysListMemoryId}[${index}] ${isInteger ? value : `${value}.0`}`];
+	});
+}
+
+function removeCode(code: string[], pressedKeysListMemoryId: string) {
+	const pattern = [`init ${pressedKeysListMemoryId}[:index] :key`];
+
+	return replaceCode(code, pattern, []);
+}
+
+export default function pianoKeyboard(store: StateManager<State>, events: EventDispatcher): () => void {
+	const state = store.getState();
+	const onCodeBlockClick = function ({ x, y, codeBlock }: CodeBlockClickEvent) {
+		const keyboard = findPianoKeyAtViewportCoordinates(state.graphicHelper, codeBlock, x, y);
+
+		if (!keyboard) {
+			return;
+		}
+
+		const key = Math.floor((x - (codeBlock.x - state.graphicHelper.viewport.x)) / keyboard.keyWidth);
+
+		if (keyboard.pressedKeys.has(key)) {
+			keyboard.pressedKeys.delete(key);
+		} else {
+			if (keyboard.pressedKeys.size === keyboard.pressedKeysListMemory.wordAlignedSize) {
+				return;
+			}
+			keyboard.pressedKeys.add(key);
+		}
+
+		const pressedNumberOfKeysMemoryLineNumber = codeBlock.code.findIndex(line => {
+			line.startsWith('int ' + keyboard.pressedNumberOfKeysMemory.id);
+		});
+
+		codeBlock.code[pressedNumberOfKeysMemoryLineNumber] =
+			'int ' + keyboard.pressedNumberOfKeysMemory.id + ' ' + keyboard.pressedKeys.size;
+
+		codeBlock.code = insertCodeAfterLine(
+			`piano ${keyboard.pressedKeysListMemory.id} ${keyboard.pressedNumberOfKeysMemory.id}`,
+			removeCode(codeBlock.code, keyboard.pressedKeysListMemory.id),
+			generateCode(
+				keyboard.pressedKeys,
+				keyboard.pressedKeysListMemory.id,
+				keyboard.pressedKeysListMemory.isInteger,
+				keyboard.startingNumber
+			)
+		);
+
+		store.set('graphicHelper.selectedCodeBlock.code', codeBlock.code);
+	};
+
+	events.on('codeBlockClick', onCodeBlockClick);
+	//events.on('mouseup', onMouseUp);
+
+	return () => {
+		events.off('codeBlockClick', onCodeBlockClick);
+		//events.off('mouseup', onMouseUp);
+	};
+}
