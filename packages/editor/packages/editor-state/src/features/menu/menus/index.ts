86957ab960dd29b92b3ce7e2c78da38c356a2e14
andorthehood <hngrhorace@gmail.com>
2026-01-14 19:04:56 +0000

    chore(editor-state): split menus into multiple files

---
+++ packages/editor/packages/editor-state/src/features/menu/menus/index.ts
@@ -0,0 +1,9 @@
+export { binaryAssetsMenu } from './binaryAssetsMenu';
+export { colorSchemeMenu } from './colorSchemeMenu';
+export { editorSettingsMenu } from './editorSettingsMenu';
+export { fontMenu } from './fontMenu';
+export { mainMenu } from './mainMenu';
+export { midiInfoMenu } from './midiInfoMenu';
+export { moduleCategoriesMenu } from './moduleCategoriesMenu';
+export { moduleMenu, type OpenGroupEvent } from './moduleMenu';
+export { projectMenu } from './projectMenu';
