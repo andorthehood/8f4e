ddfff854d8f30e8ec20908ed2c413907cdd0f860
andorthehood <andorsigned@gmail.com>
2026-01-26 21:19:38 +0100

    chore(website-background): remove unnecessary stuff

--- packages/website-background/src/editor.ts
+++ packages/website-background/src/editor.ts
@@ -1,10 +1,26 @@
-import initEditor from '@8f4e/editor';
+import initEditor, { CompilationResult, Project } from '@8f4e/editor';
+import { createMainThreadLogicRuntimeDef } from '@8f4e/runtime-main-thread-logic/runtime-def';
 
-import { runtimeRegistry, DEFAULT_RUNTIME_ID } from './runtime-registry';
-import { loadSession, loadEditorConfigBlocks } from './storage-callbacks';
-import { compileCode } from './compiler-callback';
-import compileConfig from './config-callback';
 import defaultColorScheme from './defaultColorScheme';
+import compiledProject from './project-runtime-ready.json';
+
+import type { CompiledFunctionLookup, CompiledModuleLookup } from '@8f4e/compiler';
+
+const getCodeBuffer = () => {
+	// transforms the base64 string to Uint8Array
+	const binaryString = atob(compiledProject.compiledWasm);
+	const len = binaryString.length;
+	const bytes = new Uint8Array(len);
+	for (let i = 0; i < len; i++) {
+		bytes[i] = binaryString.charCodeAt(i);
+	}
+	return bytes;
+};
+
+const getMemory = () => {
+	// For simplicity, we create a new memory with the initial size from the compiled project
+	return new WebAssembly.Memory({ initial: 1, maximum: 1 });
+};
 
 async function init() {
 	const canvas = <HTMLCanvasElement>document.getElementById('glcanvas');
@@ -12,15 +28,37 @@ async function init() {
 	canvas.height = window.innerHeight;
 	const editor = await initEditor(canvas, {
 		featureFlags: {
-			infoOverlay: true,
+			infoOverlay: false,
+			moduleDragging: false,
+			viewportDragging: false,
 		},
-		runtimeRegistry,
-		defaultRuntimeId: DEFAULT_RUNTIME_ID,
+		runtimeRegistry: {
+			MainThreadLogicRuntime: createMainThreadLogicRuntimeDef(getCodeBuffer, getMemory),
+		},
+		defaultRuntimeId: 'MainThreadLogicRuntime',
 		callbacks: {
-			compileCode: (modules, compilerOptions, functions) => compileCode(modules, compilerOptions, functions, editor),
-			compileConfig,
-			loadSession,
-			loadEditorConfigBlocks,
+			compileCode: async () => {
+				const codeBuffer = getCodeBuffer();
+				const result: CompilationResult = {
+					compiledModules: compiledProject.compiledModules as unknown as CompiledModuleLookup,
+					codeBuffer,
+					allocatedMemorySize: 2132435435,
+					hasWasmInstanceBeenReset: false,
+					memoryAction: { action: 'recreated', reason: { kind: 'no-instance' } },
+					compiledFunctions: {} as CompiledFunctionLookup,
+					byteCodeSize: codeBuffer.length,
+				};
+				return result;
+			},
+			compileConfig: async () => {
+				return { config: compiledProject.compiledProjectConfig, errors: [] };
+			},
+			loadSession: async () => {
+				return compiledProject as unknown as Project;
+			},
+			loadEditorConfigBlocks: async () => {
+				return null;
+			},
 			getListOfColorSchemes: async () => ['default'],
 			getColorScheme: async () => defaultColorScheme,
 		},
