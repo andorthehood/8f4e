3e1742c1b37bfc5f195984ee37b7679f34f1556a
andorthehood <hngrhorace@gmail.com>
2026-02-03 21:42:41 +0100

    compiler: add float support for edge detectors

--- packages/compiler/tests/instructions/fallingEdge.test.ts
+++ packages/compiler/tests/instructions/fallingEdge.test.ts
@@ -30,3 +30,34 @@ moduleEnd
 		[{ input: 10 }, { output: 0 }],
 	]
 );
+
+moduleTester(
+	'fallingEdge (float)',
+	`module fallingEdgeFloat
+
+float input
+int output
+
+push &output
+ push input
+ fallingEdge
+ if int
+  push 1
+ else
+  push 0
+ ifEnd
+store
+
+moduleEnd
+`,
+	[
+		[{ input: 10.1 }, { output: 0 }],
+		[{ input: 11.2 }, { output: 0 }],
+		[{ input: 12.3 }, { output: 0 }],
+		[{ input: 9.9 }, { output: 1 }],
+		[{ input: 12.2 }, { output: 0 }],
+		[{ input: 12.2 }, { output: 0 }],
+		[{ input: 10.8 }, { output: 1 }],
+		[{ input: 10.8 }, { output: 0 }],
+	]
+);
