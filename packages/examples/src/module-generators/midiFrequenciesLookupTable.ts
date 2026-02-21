45f3c79221e015ca02b906f8737887875b19511e
andorthehood <hngrhorace@gmail.com>
2026-02-21 10:54:53 +0100

    feat: add lookup tables for exponential, MIDI frequencies, and sine functions
    - Implemented expLookupTable.ts to generate an exponential function lookup table.
    - Created midiFrequenciesLookupTable.ts for MIDI note frequencies.
    - Developed sineLookupTable.ts for sine function values.
    - Added generateModules.ts to automate the generation of module files.
    - Updated module files for expLookupTable, midiFrequenciesLookupTable, and sineLookupTable with precomputed values.
    - Modified tsconfig.json to allow importing TypeScript extensions and rewrite relative import extensions.

---
+++ packages/examples/src/module-generators/midiFrequenciesLookupTable.ts
@@ -0,0 +1,23 @@
+export const midiFrequenciesLookupTableMetadata = {
+	title: 'MIDI Frequencies Lookup Table',
+	category: 'Lookup Tables',
+} as const;
+
+const A = 440;
+
+const midiFrequencies = new Array(128).fill(0).map((value, note) => {
+	return (A / 32) * 2 ** ((note - 9) / 12);
+});
+
+const midiFrequenciesLookupTable = `module midiLUT
+
+float[] notes 128
+${midiFrequencies
+	.map((value, note) => {
+		return `init notes[${note}] ${value.toFixed(4)}`;
+	})
+	.join('\n')}
+
+moduleEnd`;
+
+export default midiFrequenciesLookupTable;
