330168e2591cea1723c97adb61c426c7dbd0bcf3
andorthehood <hngrhorace@gmail.com>
2026-03-13 19:26:44 +0100

    fix(editor-state): refine hide directive behavior

--- packages/editor/packages/editor-state/src/features/code-blocks/features/directives/hide/plugin.ts
+++ packages/editor/packages/editor-state/src/features/code-blocks/features/directives/hide/plugin.ts
@@ -1,13 +1,7 @@
-import { getBlockType } from '@8f4e/compiler/syntax';
-
 import { createDirectivePlugin } from '../utils';
 
 export default createDirectivePlugin('hide', (directive, draft) => {
 	if (directive.args.length > 0) {
-		return;
-	}
-
-	if (getBlockType(draft.sourceCode) !== 'module') {
 		return;
 	}
 
