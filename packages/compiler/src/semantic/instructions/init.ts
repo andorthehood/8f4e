eb99dc0ed1cf4f8fd21162fa5768196c0acadbe3
andorthehood <hngrhorace@gmail.com>
2026-03-27 20:52:55 +0100

    refactor(compiler): resolve intermodule defaults in semantics

--- packages/compiler/src/semantic/instructions/init.ts
+++ packages/compiler/src/semantic/instructions/init.ts
@@ -1,14 +1,8 @@
-import {
-	INTERMODULAR_REFERENCE_PATTERN,
-	isIntermodularElementCountReference,
-	isIntermodularElementMaxReference,
-	isIntermodularElementMinReference,
-	isIntermodularElementWordSizeReference,
-	isIntermodularModuleReference,
-} from '@8f4e/ast-parser';
+import { INTERMODULAR_REFERENCE_PATTERN } from '@8f4e/ast-parser';
 
 import { ErrorCode, getError } from '../../compilerError';
 import { ArgumentType, type AST, type CompilationContext } from '../../types';
+import resolveIntermodularReferenceValue from '../../utils/resolveIntermodularReferenceValue';
 
 export default function semanticInit(line: AST[number], context: CompilationContext) {
 	if (!line.arguments[0] || !line.arguments[1]) {
@@ -33,26 +27,26 @@ export default function semanticInit(line: AST[number], context: CompilationCont
 		defaultValue = line.arguments[1].value;
 	} else if (
 		line.arguments[1].type === ArgumentType.IDENTIFIER &&
-		(INTERMODULAR_REFERENCE_PATTERN.test(line.arguments[1].value) ||
-			isIntermodularModuleReference(line.arguments[1].value) ||
-			isIntermodularElementCountReference(line.arguments[1].value) ||
-			isIntermodularElementWordSizeReference(line.arguments[1].value) ||
-			isIntermodularElementMaxReference(line.arguments[1].value) ||
-			isIntermodularElementMinReference(line.arguments[1].value))
+		INTERMODULAR_REFERENCE_PATTERN.test(line.arguments[1].value)
 	) {
-		return;
-	} else if (line.arguments[1].type === ArgumentType.IDENTIFIER && line.arguments[1].value[0] === '&') {
-		const referencedMemoryItem = context.namespace.memory[line.arguments[1].value.substring(1)];
-
-		if (!referencedMemoryItem) {
-			throw getError(ErrorCode.UNDECLARED_IDENTIFIER, line, context, {
-				identifier: line.arguments[1].value.substring(1),
-			});
-		}
-
-		defaultValue = referencedMemoryItem.byteAddress;
+		defaultValue = resolveIntermodularReferenceValue(line.arguments[1].value, line, context) ?? 0;
 	} else if (line.arguments[1].type === ArgumentType.IDENTIFIER) {
-		return;
+		const intermodularValue = resolveIntermodularReferenceValue(line.arguments[1].value, line, context);
+		if (typeof intermodularValue === 'number') {
+			defaultValue = intermodularValue;
+		} else if (line.arguments[1].value[0] === '&') {
+			const referencedMemoryItem = context.namespace.memory[line.arguments[1].value.substring(1)];
+
+			if (!referencedMemoryItem) {
+				throw getError(ErrorCode.UNDECLARED_IDENTIFIER, line, context, {
+					identifier: line.arguments[1].value.substring(1),
+				});
+			}
+
+			defaultValue = referencedMemoryItem.byteAddress;
+		} else {
+			return;
+		}
 	} else {
 		throw getError(ErrorCode.EXPECTED_VALUE, line, context);
 	}
