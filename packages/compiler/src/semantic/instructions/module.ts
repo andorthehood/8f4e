ff9c98801787eab684446aec6a4a75544a87ab89
andorthehood <hngrhorace@gmail.com>
2026-03-28 12:38:45 +0100

    refactor(compiler): remove test-only ast defensive guards

--- packages/compiler/src/semantic/instructions/module.ts
+++ packages/compiler/src/semantic/instructions/module.ts
@@ -1,13 +1,15 @@
 import { ArgumentType, BLOCK_TYPE, type AST, type CompilationContext } from '../../types';
 
 export default function semanticModule(line: AST[number], context: CompilationContext) {
+	const moduleId = (line.arguments[0] as Extract<(typeof line.arguments)[number], { type: ArgumentType.IDENTIFIER }>)
+		.value;
+
 	context.blockStack.push({
 		hasExpectedResult: false,
 		expectedResultIsInteger: false,
 		blockType: BLOCK_TYPE.MODULE,
 	});
 
-	const moduleId = (line.arguments[0] as { type: ArgumentType.IDENTIFIER; value: string }).value;
 	context.namespace.moduleName = moduleId;
 	context.codeBlockId = moduleId;
 	context.codeBlockType = 'module';
