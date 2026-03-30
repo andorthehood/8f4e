ebd2bc8dfa99b7b303a1788583ae7015ba2ebacd
andorthehood <hngrhorace@gmail.com>
2026-03-30 17:14:46 +0200

    sprite-generator: remove hardcoded ascii lookup arrays

--- packages/editor/packages/sprite-generator/src/font.ts
+++ packages/editor/packages/sprite-generator/src/font.ts
@@ -5,144 +5,8 @@ import { Command, DrawingCommand, ColorScheme } from './types';
 const offsetX = 0;
 const offsetY = 0;
 
-const asciiCodes = [
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-	32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-	61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
-	90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114,
-	115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
-] as const;
-
-const asciiChars = [
-	'\u0000',
-	'\u0001',
-	'\u0002',
-	'\u0003',
-	'\u0004',
-	'\u0005',
-	'\u0006',
-	'\u0007',
-	'\b',
-	'\t',
-	'\n',
-	'\u000b',
-	'\f',
-	'\r',
-	'\u000e',
-	'\u000f',
-	'\u0010',
-	'\u0011',
-	'\u0012',
-	'\u0013',
-	'\u0014',
-	'\u0015',
-	'\u0016',
-	'\u0017',
-	'\u0018',
-	'\u0019',
-	'\u001a',
-	'\u001b',
-	'\u001c',
-	'\u001d',
-	'\u001e',
-	'\u001f',
-	' ',
-	'!',
-	'"',
-	'#',
-	'$',
-	'%',
-	'&',
-	"'",
-	'(',
-	')',
-	'*',
-	'+',
-	',',
-	'-',
-	'.',
-	'/',
-	'0',
-	'1',
-	'2',
-	'3',
-	'4',
-	'5',
-	'6',
-	'7',
-	'8',
-	'9',
-	':',
-	';',
-	'<',
-	'=',
-	'>',
-	'?',
-	'@',
-	'A',
-	'B',
-	'C',
-	'D',
-	'E',
-	'F',
-	'G',
-	'H',
-	'I',
-	'J',
-	'K',
-	'L',
-	'M',
-	'N',
-	'O',
-	'P',
-	'Q',
-	'R',
-	'S',
-	'T',
-	'U',
-	'V',
-	'W',
-	'X',
-	'Y',
-	'Z',
-	'[',
-	'\\',
-	']',
-	'^',
-	'_',
-	'`',
-	'a',
-	'b',
-	'c',
-	'd',
-	'e',
-	'f',
-	'g',
-	'h',
-	'i',
-	'j',
-	'k',
-	'l',
-	'm',
-	'n',
-	'o',
-	'p',
-	'q',
-	'r',
-	's',
-	't',
-	'u',
-	'v',
-	'w',
-	'x',
-	'y',
-	'z',
-	'{',
-	'|',
-	'}',
-	'~',
-	'',
-] as const;
+const ASCII_START = 0;
+const ASCII_END = 127;
 
 const colorNames: Array<keyof ColorScheme['text']> = [
 	'lineNumber',
@@ -220,14 +84,18 @@ export function drawCharacterMatrix(
 }
 
 function generateFont(x = 0, y = 0, font: number[], characterWidth: number, characterHeight: number): DrawingCommand[] {
-	return [
-		[Command.TRANSLATE, x, y],
-		...asciiCodes.flatMap(code => [
-			...drawCharacter(font, code, characterWidth, characterHeight),
-			[Command.TRANSLATE, characterWidth, 0] as DrawingCommand,
-		]),
-		[Command.RESET_TRANSFORM],
-	];
+	const commands: DrawingCommand[] = [[Command.TRANSLATE, x, y]];
+
+	for (let code = ASCII_START; code <= ASCII_END; code++) {
+		commands.push(...drawCharacter(font, code, characterWidth, characterHeight), [
+			Command.TRANSLATE,
+			characterWidth,
+			0,
+		]);
+	}
+
+	commands.push([Command.RESET_TRANSFORM]);
+	return commands;
 }
 
 export default function generateFonts(
@@ -252,33 +120,26 @@ function capitalize(word: string) {
 }
 
 export type FontLookups = {
-	[key in keyof ColorScheme['text'] as `font${Capitalize<string & key>}`]: Record<
-		(typeof asciiChars)[number] | (typeof asciiCodes)[number],
-		SpriteCoordinates
-	>;
+	[key in keyof ColorScheme['text'] as `font${Capitalize<string & key>}`]: Record<number | string, SpriteCoordinates>;
 };
 
 export const generateLookups = function (characterWidth: number, characterHeight: number) {
 	return Object.fromEntries(
 		colorNames.map(colorName => {
-			return [
-				`font` + capitalize(colorName),
+			const lookups: Record<number | string, SpriteCoordinates> = {};
 
-				Object.fromEntries(
-					[...asciiCodes, ...asciiChars].map(char => {
-						const code = typeof char === 'string' ? char.charCodeAt(0) : char;
-						return [
-							char,
-							{
-								x: code * characterWidth + offsetX,
-								y: generateFontPositions(characterHeight)[colorName],
-								spriteHeight: characterHeight,
-								spriteWidth: characterWidth,
-							},
-						];
-					})
-				),
-			];
+			for (let code = ASCII_START; code <= ASCII_END; code++) {
+				const coordinates = {
+					x: code * characterWidth + offsetX,
+					y: generateFontPositions(characterHeight)[colorName],
+					spriteHeight: characterHeight,
+					spriteWidth: characterWidth,
+				};
+				lookups[code] = coordinates;
+				lookups[String.fromCharCode(code)] = coordinates;
+			}
+
+			return [`font` + capitalize(colorName), lookups];
 		})
 	) as FontLookups;
 };
