f9a5ab6d9abcf1cf43f16d70616cf3e506235d6a
andorthehood <petrina@polgarhivatal.nl>
2026-05-30 23:19:52 +0200

    feat(editor): draw shared entry outlines
    Co-authored-by: Futaki <futaki@polgarhivatal.nl>

---
+++ packages/editor/packages/editor-state/src/features/code-blocks/features/entryOutlines/effect.ts
@@ -0,0 +1,24 @@
+import { StateManager } from '@8f4e/state-manager';
+
+import deriveEntryOutlines from './deriveEntryOutlines';
+
+import type { State } from '@8f4e/editor-state-types';
+
+export default function entryOutlines(store: StateManager<State>): void {
+	const state = store.getState();
+
+	function syncEntryOutlines(): void {
+		store.set(
+			'graphicHelper.entryOutlines',
+			deriveEntryOutlines(state.graphicHelper.codeBlocks, state.viewport.vGrid * 8, state.viewport.hGrid * 4)
+		);
+	}
+
+	store.subscribe('graphicHelper.codeBlocks', syncEntryOutlines);
+	store.subscribe('graphicHelper.selectedCodeBlock.code', syncEntryOutlines);
+	store.subscribe('graphicHelper.selectedCodeBlockForProgrammaticEdit.code', syncEntryOutlines);
+	store.subscribe('graphicHelper.selectedCodeBlockForProgrammaticEditWithoutCompilerTrigger', syncEntryOutlines);
+	store.subscribe('graphicHelper.selectedCodeBlockForProgrammaticEditWithoutCompilerTrigger.code', syncEntryOutlines);
+
+	syncEntryOutlines();
+}
