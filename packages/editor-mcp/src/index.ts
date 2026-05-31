c91acdb0ce16cb73aa83aa67f30aada0b3a93d17
andorthehood <hngrhorace@gmail.com>
2026-05-31 02:06:02 +0200

    chore: remove pmml28f4e package and related files

--- packages/editor-mcp/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { createEditorSession } from './session';
-export type { BrowserOptions, BrowserSession, CodeBlockSummary } from './session';
-export { read8f4eManual } from './manual';
-export type { EightF4eManual, ManualSection } from './manual';
